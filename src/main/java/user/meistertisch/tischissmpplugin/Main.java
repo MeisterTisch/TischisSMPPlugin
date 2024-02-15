c59728caca9b8e2b8b4128dfffe5ace679cd6d03
Daniel Tischbein <tischesindtoll@outlook.de>
2024-02-15 11:03:24 +0100

    Set up Main

--- src/main/java/user/meistertisch/tischissmpplugin/Main.java
+++ src/main/java/user/meistertisch/tischissmpplugin/Main.java
@@ -1,17 +1,35 @@
 package user.meistertisch.tischissmpplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    private static Main plugin;
+    PluginManager pluginManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        //First
+        plugin = this;
+        pluginManager = Bukkit.getPluginManager();
+
+        //Commands
+
+
+        //Listeners
+
+
+        //Misc
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Main getPlugin() {
+        return plugin;
     }
 }
