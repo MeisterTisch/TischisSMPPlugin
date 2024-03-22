ef58158318ca5601443832db5be2899f3c5ac33a
Daniel Tischbein <tischesindtoll@outlook.de>
2024-03-22 12:41:11 +0100

    For CM5

--- src/main/java/user/meistertisch/tischissmpplugin/admin/CommandStart.java
+++ src/main/java/user/meistertisch/tischissmpplugin/admin/CommandStart.java
@@ -5,14 +5,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import user.meistertisch.tischissmpplugin.Main;
+import user.meistertisch.tischissmpplugin.players.FilePlayers;
 
 import java.util.List;
 
 public class CommandStart implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player player && player.getDisplayName().equals("MeisterTisch")){
-            Main.getPlugin().getConfig().set("isStarted", true);
+        if(commandSender instanceof Player player && FilePlayers.getConfig().getBoolean(player.getDisplayName() + ".isAdmin")
+                && player.getDisplayName().equals("MeisterTisch")){
+            Main.getPlugin().getConfig().set("isStarted", !Main.getPlugin().getConfig().getBoolean("isStarted"));
+            player.sendMessage("ist nun: " + Main.getPlugin().getConfig());
             Main.getPlugin().saveConfig();
             Main.getPlugin().reloadConfig();
         }
