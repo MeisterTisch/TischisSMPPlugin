2abf2230eb0711a8739e8eb272aa47e8b9a4f6a8
Daniel B. <boger.daniel@outlook.de>
2024-05-25 08:51:06 +0200

    Updated FreezingPlayer and ListenerFreeze (instead of Player Object the UUID will be saved)

--- src/main/java/user/meistertisch/tischissmpplugin/admin/freezing/FreezingPlayer.java
+++ src/main/java/user/meistertisch/tischissmpplugin/admin/freezing/FreezingPlayer.java
@@ -4,19 +4,20 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class FreezingPlayer {
-    private static List<Player> playerList = new ArrayList<>();
+    private static List<UUID> playerList = new ArrayList<>();
 
     public static void addPlayer(Player player){
-        playerList.add(player);
+        playerList.add(player.getUniqueId());
     }
 
     public static void removePlayer(Player player){
-        playerList.remove(player);
+        playerList.remove(player.getUniqueId());
     }
 
-    public static List<Player> getPlayerList() {
+    public static List<UUID> getPlayerList() {
         return playerList;
     }
 }
--- src/main/java/user/meistertisch/tischissmpplugin/admin/freezing/ListenerFreeze.java
+++ src/main/java/user/meistertisch/tischissmpplugin/admin/freezing/ListenerFreeze.java
@@ -11,28 +11,28 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class ListenerFreeze implements Listener {
     @EventHandler
     public void targetMoves(PlayerMoveEvent event){
-        if(FreezingPlayer.getPlayerList().contains(event.getPlayer())){
+        if(FreezingPlayer.getPlayerList().contains(event.getPlayer().getUniqueId())){
             event.setCancelled(true);
         }
     }
 
     @EventHandler
     public void targetGetsDamage(EntityDamageEvent event){
-        if(event.getEntity() instanceof Player player && FreezingPlayer.getPlayerList().contains(player)){
+        if(event.getEntity() instanceof Player player && FreezingPlayer.getPlayerList().contains(player.getUniqueId())){
             event.setCancelled(true);
         }
     }
 
     @EventHandler
     public void targetBuilds(BlockPlaceEvent event){
-        if(FreezingPlayer.getPlayerList().contains(event.getPlayer())){
+        if(FreezingPlayer.getPlayerList().contains(event.getPlayer().getUniqueId())){
             event.setCancelled(true);
         }
     }
 
     @EventHandler
     public void targetBreaks(BlockBreakEvent event){
-        if(FreezingPlayer.getPlayerList().contains(event.getPlayer())){
+        if(FreezingPlayer.getPlayerList().contains(event.getPlayer().getUniqueId())){
             event.setCancelled(true);
         }
     }
